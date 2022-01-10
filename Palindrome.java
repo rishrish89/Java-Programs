c73a89b8737c6feb65379336d308faa80679aa46
rishrish89 <70275962+rishrish89@users.noreply.github.com>
2022-01-10 20:18:37 +0530

    check whether a given number is a palindrome.
    Implement a program to check whether a given number is a palindrome.
    
    Palindrome is a sequence that reads the same backwards as forwards.
    
    E.g.: 121, 1331, 2332, 78900987, 123456654321,  etc.

--- Palindrome.java
+++ Palindrome.java
@@ -1,9 +1,9 @@
 class Tester {
 	public static void main(String[] args) {
-		int inp = 1332;
-		int ip = inp;
-		int pop = 0;
-		while(ip > 0){
+	long inp = 123456654321L;
+    long ip = inp;
+    long pop = 0L;
+		while(ip > (long)0){
 		     pop = pop *10+ ip % 10;
 		     ip = ip /10;
 		     }
